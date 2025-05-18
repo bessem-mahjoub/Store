407e2502793ca473280a52fcf5f524ec5115588c
bessem-mahjoub <bassem.mahjoub13@gmail.com>
2025-05-19 00:02:41 +0200

    Ajout de Test Unitaire et Mockito

---
+++ src/test/java/MonServiceTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class MonServiceTest {
+    @Test
+    public void testAddition() {
+        int somme = 2 + 3;
+        assertEquals(5, somme, "L'addition doit être correcte");
+    }
+}
